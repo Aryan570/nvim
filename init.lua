8b853b264108b89c8d744b5538d05b1e96abe696
Aryan <malhotraaryan570@gmail.com>
2024-12-07 14:35:46 +0530

    rust_stuff

--- init.lua
+++ init.lua
@@ -90,11 +90,13 @@ require("lazy").setup({
 			config = function()
 				require("telescope").setup({})
 				require("telescope").load_extension("fzf")
+				require("telescope").load_extension("file_browser")
 				pcall(require("telescope").load_extension, "ui-select")
 				local builtin = require("telescope.builtin")
 				vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 				vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 				vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+				--vim.keymap.set("n", "<leader>sf", ":Telescope find_files path=%:p:h <CR>")
 				vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 				vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 				vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
@@ -102,6 +104,8 @@ require("lazy").setup({
 				vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 				vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 				vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+				vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+				--vim.keymap.set("n", "<space>fb", ":Telescope file_browser<CR>")
 				vim.keymap.set("n", "<leader>sn", function()
 					builtin.find_files({ cwd = vim.fn.stdpath("config") })
 				end, { desc = "[S]earch [N]eovim files" })
@@ -136,6 +140,21 @@ require("lazy").setup({
 				},
 				indent = { enable = true },
 			},
+		},
+		{
+			"mrcjkb/rustaceanvim",
+			version = "^5", -- Recommended
+			lazy = false, -- This plugin is already lazy
+			["rust_analyzer"] = {
+				cargo = {
+					allFeatures = true,
+				},
+			},
+		},
+
+		{
+			"nvim-telescope/telescope-file-browser.nvim",
+			dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
 		},
 		{
 			"folke/which-key.nvim",
@@ -792,7 +811,7 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
 	callback = function(event)
 		local title = "vim"
 		if event.file ~= "" then
-			title = string.format("vim: %s", vim.fs.basename(event.file))
+			title = string.format("%s", vim.fs.basename(event.file))
 		end
 
 		vim.fn.system({ "wezterm", "cli", "set-tab-title", title })
@@ -806,3 +825,4 @@ vim.api.nvim_create_autocmd({ "VimLeave" }, {
 		vim.fn.system({ "wezterm", "cli", "set-tab-title", "Wezterm" })
 	end,
 })
+vim.g.autoformat = false
