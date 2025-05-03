20497b2938337877d8b5984806d4ec6ecdf05cd9
Aryan <malhotraaryan570@gmail.com>
2025-05-03 15:41:51 +0530

    new_config

--- init.lua
+++ init.lua
@@ -26,7 +26,7 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup lazy.nvim
 require("lazy").setup({
 	spec = {
-		{
+		--[[		{
 			"scottmckendry/cyberdream.nvim",
 			lazy = false,
 			priority = 1000,
@@ -72,6 +72,96 @@ require("lazy").setup({
 				})
 				vim.cmd("colorscheme cyberdream")
 				-- vim.api.nvim_set_keymap("n", "<leader>tt", ":CyberdreamToggleMode<CR>", { noremap = true, silent = true })
+			end,
+		},]]
+		--
+		{
+			"rose-pine/neovim",
+			name = "rose-pine",
+			config = function()
+				require("rose-pine").setup({
+					variant = "auto", -- auto, main, moon, or dawn
+					dark_variant = "main", -- main, moon, or dawn
+					dim_inactive_windows = false,
+					extend_background_behind_borders = true,
+
+					enable = {
+						terminal = true,
+						legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+						migrations = true, -- Handle deprecated options automatically
+					},
+
+					styles = {
+						bold = true,
+						italic = true,
+						transparency = true,
+					},
+
+					groups = {
+						border = "muted",
+						link = "iris",
+						panel = "surface",
+
+						error = "love",
+						hint = "iris",
+						info = "foam",
+						note = "pine",
+						todo = "rose",
+						warn = "gold",
+
+						git_add = "foam",
+						git_change = "rose",
+						git_delete = "love",
+						git_dirty = "rose",
+						git_ignore = "muted",
+						git_merge = "iris",
+						git_rename = "pine",
+						git_stage = "iris",
+						git_text = "rose",
+						git_untracked = "subtle",
+
+						h1 = "iris",
+						h2 = "foam",
+						h3 = "rose",
+						h4 = "gold",
+						h5 = "pine",
+						h6 = "foam",
+					},
+
+					palette = {
+						-- Override the builtin palette per variant
+						-- moon = {
+						--     base = '#18191a',
+						--     overlay = '#363738',
+						-- },
+					},
+
+					-- NOTE: Highlight groups are extended (merged) by default. Disable this
+					-- per group via `inherit = false`
+					highlight_groups = {
+						-- Comment = { fg = "foam" },
+						-- StatusLine = { fg = "love", bg = "love", blend = 15 },
+						-- VertSplit = { fg = "muted", bg = "muted" },
+						-- Visual = { fg = "base", bg = "text", inherit = false },
+					},
+
+					before_highlight = function(group, highlight, palette)
+						-- Disable all undercurls
+						-- if highlight.undercurl then
+						--     highlight.undercurl = false
+						-- end
+						--
+						-- Change palette colour
+						-- if highlight.fg == palette.pine then
+						--     highlight.fg = palette.foam
+						-- end
+					end,
+				})
+
+				vim.cmd("colorscheme rose-pine")
+				-- vim.cmd("colorscheme rose-pine-main")
+				-- vim.cmd("colorscheme rose-pine-moon")
+				-- vim.cmd("colorscheme rose-pine-dawn")
 			end,
 		},
 		-- { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
@@ -141,17 +231,6 @@ require("lazy").setup({
 				indent = { enable = true },
 			},
 		},
-		{
-			"mrcjkb/rustaceanvim",
-			version = "^5", -- Recommended
-			lazy = false, -- This plugin is already lazy
-			["rust_analyzer"] = {
-				cargo = {
-					allFeatures = true,
-				},
-			},
-		},
-
 		{
 			"nvim-telescope/telescope-file-browser.nvim",
 			dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
@@ -252,6 +331,7 @@ require("lazy").setup({
 				require("mini.surround").setup()
 				require("mini.files").setup()
 				require("mini.clue").setup()
+				require("mini.pairs").setup()
 				--local statusline = require 'mini.statusline'
 				--statusline.setup {use_icons = vim.g.have_nerd_font}
 			end,
@@ -546,7 +626,7 @@ require("lazy").setup({
 					-- Disable "format_on_save lsp_fallback" for languages that don't
 					-- have a well standardized coding style. You can add additional
 					-- languages here or re-enable it for the disabled ones.
-					local disable_filetypes = { c = true, cpp = true }
+					local disable_filetypes = { c = true, cpp = true, rust = false, lua = false }
 					local lsp_format_opt
 					if disable_filetypes[vim.bo[bufnr].filetype] then
 						lsp_format_opt = "never"
@@ -825,4 +905,3 @@ vim.api.nvim_create_autocmd({ "VimLeave" }, {
 		vim.fn.system({ "wezterm", "cli", "set-tab-title", "Wezterm" })
 	end,
 })
-vim.g.autoformat = false
